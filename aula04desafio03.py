60efdc7e99f37975ee2f194e607f148723a0cbc8
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 20:20:56 -0300

    Create aula04desafio03.py

---
+++ aula04desafio03.py
@@ -0,0 +1,5 @@
+#cire um script python que leia dois números e tente mostrar a soma entre eles
+num1=input("Digite o primeiro número ")
+num2=input("Digite o segundo número ")
+soma=int(num1)+int(num2)
+print("A soma é ",soma)
