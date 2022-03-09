01b353208652db786dd2778ddca089888f927951
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 20:20:25 -0300

    Update aula06.py

--- aula06.py
+++ aula06.py
@@ -1,3 +1,6 @@
+#aula 06 tipos primitivos e saída de dados
+#resolução do desafio 03 da aula 04 (cire um script
+# python que leia dois números e tente mostrar a soma entre eles)
 n1=int(input("Digite um número "))
 n2=int(input("Digite outro número "))
 s=n1+n2
