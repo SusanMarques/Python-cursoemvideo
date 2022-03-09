9b9511cec3b0fe24ed420b03b0183261eb684efc
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 11:50:35 -0300

    Create desafio02.py

---
+++ desafio02.py
@@ -0,0 +1,4 @@
+diadenasc=input("Qual o dia do seu aniversário?")
+mesdenasc=input("Qual o mês do seu aniversário?")
+anodenasc=input("Qual o ano do seu aniversário?")
+print("A data do seu aniversário é ",diadenasc,"/",mesdenasc,"/",anodenasc,)
