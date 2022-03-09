fb2f64b8c02a83ac440519814e8715332f81d8fd
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 20:19:55 -0300

    Delete desafio02.py

--- desafio02.py
+++
@@ -1,4 +0,0 @@
-diadenasc=input("Qual o dia do seu aniversário?")
-mesdenasc=input("Qual o mês do seu aniversário?")
-anodenasc=input("Qual o ano do seu aniversário?")
-print("Você nasceu no dia ",diadenasc," de ",mesdenasc," de ",anodenasc,". Correto? ")
