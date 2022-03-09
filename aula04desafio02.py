9fa349ed166f1e504e733d72bb8087e09ba10505
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 20:21:01 -0300

    Create aula04desafio02.py

---
+++ aula04desafio02.py
@@ -0,0 +1,6 @@
+#crie um script python que leia o dia, o mês e o ano de nascimento de
+# uma pessoa e mostre uma mensagem com a data formatada
+diadenasc=input("Qual o dia do seu aniversário?")
+mesdenasc=input("Qual o mês do seu aniversário?")
+anodenasc=input("Qual o ano do seu aniversário?")
+print("Você nasceu no dia ",diadenasc," de ",mesdenasc," de ",anodenasc,". Correto? ")
