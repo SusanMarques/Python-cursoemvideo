e020a46e2b71d24aa17d3a850b5ab218e0533735
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 23:31:51 -0300

    Update ex003.py

--- LISTA DE EXERCICIOS curso em video/ex003.py
+++ LISTA DE EXERCICIOS curso em video/ex003.py
@@ -1,1 +1,9 @@
-#somando dois números
+'''
+somando dois números
+ex 003: crie um programa que leia dois números e mostre a soma entre eles
+
+'''
+n1=int(input("Digite um número "))
+n2=int(input("Digite outro número "))
+soma=n1+n2
+print("A soma entre {} e {} é igual a {} ".format(n1,n2,soma))
