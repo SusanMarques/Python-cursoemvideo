7294176de9d11acd9e753500fa4f7fa46a4d29c1
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-09 23:09:42 -0300

    Update aula06desafio2.py

--- aula06desafio2.py
+++ aula06desafio2.py
@@ -1,6 +1,17 @@
 #faça um programa que leia algo pelo teclado e mostre na tela 
 # o seu tipo primitivo e todas as informações possíveis sobre ele
+print("")
 leitor=input("Digite alguma coisa: ")
+print("")
+#tipos
+tipo=type(leitor)
+#oque
 numero=bool(leitor.isnumeric())
 letra=bool(leitor.isalpha())
-letra_num=(leitor.isalphanum)
+letra_numero=bool(leitor.isalpha())
+minusculo=bool(leitor.islower())
+maiusculo=bool(leitor.isupper())
+todas_as_iniciais_maiusculo=bool(leitor.istitle())
+print(f"topo de variavel {tipo}")
+print(f"contem números{numero}\n contem letras{letra}\n contem letras com numeros{letra_numero}\n E maiuscula{maiusculo}\n É minusculo {todas_as_iniciais_maiusculo}")
+
