148ec833000d6bb0831b000bc583a42795a96a49
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-14 12:42:17 -0300

    Create aula07desafio01.py

---
+++ aula07desafio01.py
@@ -0,0 +1,10 @@
+'''
+Faça um programa que leia um número inteiro 
+e mostre na tela o seu sucessor e o seu antecessor
+
+'''
+n1= int(input("Digite um número inteiro "))
+n2=1
+sucessor= n1+n2
+antecessor=n1-n2
+print("O sucessor é {}, e o antecessor é {}".format(sucessor,antecessor))
