fd21c7a08fc637985c6d410411c2c4b4365571cf
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-14 12:28:08 -0300

    Create aula07.py

---
+++ aula07.py
@@ -0,0 +1,9 @@
+n1=int(input("digite um número "))
+n2=int(input("digite outro número "))
+soma= n1+ n2
+sub = n1-n2
+mult= n1* n2
+div= n1 / n2
+div2= n1// n2
+ex= n1**n2
+print("A soma vale {}, a subtração vale {}, a multiplicação vale {}, \n a divisão vale {}, a divisão inteira {}, a exponenciação vale {}".format(soma,sub,mult,div,div2,ex))
