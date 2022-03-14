6b6f6229cb6e73c77d4bceca0c747434e1680386
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-14 14:01:10 -0300

    Create aula07desafio06.py

---
+++ aula07desafio06.py
@@ -0,0 +1,18 @@
+'''
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira 
+e mostre quantos dólares ela pode comprar
+quanto valia o dólar na época do vídeo>> US$ 1  = R$ 3,27
+
+'''
+
+din=float(input("Digite quanto tem na carteira: "))
+dolar= din/3.27
+print("Com R$ {} Você pode comprar US$ {}".format(din,dolar))
+
+'''
+Quanto vale o dólar hoje >> US$ 1 = R$ 5,10
+
+'''
+din = float(input("Digite quanto tem na carteira "))
+dolar= din/5.10
+print("Com R$ {} é possível comprar U$ {}".format(din,dolar))
