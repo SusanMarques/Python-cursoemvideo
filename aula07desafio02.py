d3c4bc1302344af6998a0f5de11ab72decda1ea8
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-14 13:03:04 -0300

    Create aula07desafio02.py

---
+++ aula07desafio02.py
@@ -0,0 +1,10 @@
+'''
+Crie um algoritmo que leia um número
+e mostre o seu dobro, o seu triplo e raiz quadrada.
+'''
+
+num=int(input("Digite um número "))
+dobro= num*2
+triplo=num*3
+raizq= num**(0.5)
+print("O dobro do número {}, é igual a {}, o triplo é {} e a raiz quadrada é {} ".format(num,dobro,triplo,raizq))
