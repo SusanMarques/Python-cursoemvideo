ccecfd53f0d52eb1d11420438109aa802a9dd93c
SusanMarques <100443975+SusanMarques@users.noreply.github.com>
2022-03-14 13:03:11 -0300

    Create aula07desafio03.py

---
+++ aula07desafio03.py
@@ -0,0 +1,8 @@
+'''
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+'''
+nota1=int(input("Digite a primeira nota "))
+nota2=int(input("Digite a segunda nota "))
+média= (nota1+nota2)/2
+print("A média do aluno é {}".format(média))
